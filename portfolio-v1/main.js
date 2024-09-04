f37b608c6631eac6b6924ba77b14c3d979cc7b1a
ChenoGG <65572462+ChenoGG@users.noreply.github.com>
2024-09-04 03:06:33 +0200

    Add html-form button functionality

--- portfolio-v1/main.js
+++ portfolio-v1/main.js
@@ -1,1 +1,15 @@
 import './style.css'
+
+// Create project + html form functions
+const createProjectButton = document.getElementById("create-button")
+const exitProjectButton = document.getElementById("exit-button")
+const formPopUp = document.getElementById("project-form")
+
+createProjectButton.addEventListener("click", () => {
+    formPopUp.style.display = "block"
+})
+
+exitProjectButton.addEventListener("click", () => {
+    formPopUp.style.display = "none"
+})
+
