a47977d0fbb8cab875a6b5f56ab6d4fa10d4b643
ChenoGG <65572462+ChenoGG@users.noreply.github.com>
2024-09-04 04:27:05 +0200

    fix: correct path and add readFile

--- portfolio-v1/server/server.js
+++ portfolio-v1/server/server.js
@@ -2,6 +2,7 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { serveStatic } from "@hono/node-server/serve-static";
+import { readFile } from "node:fs/promises";
 
 const app = new Hono();
 
@@ -9,15 +10,21 @@ app.use("/*", cors())
 
 app.use("/statics/*", serveStatic({ root: "./" }))
 
+// /json is the web-route
 app.get("/json", async (c) => {
-    const data = await readFile("/assets/data.json", "utf-8")
+    const data = await readFile("assets/data.json", "utf-8")
 
     return c.json(JSON.parse(data))
 })
 
+// Server should work
+app.get("/", async (c) => {
+    return c.text("Hellooooo am I breaking the 404?");
+});
+
 console.log("HOPE IT WORKS LUL")
 
-const port  = 3999
+const port = 3999
 serve({
     fetch: app.fetch,
     port,
