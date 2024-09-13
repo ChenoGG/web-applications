1dda5186e4437a3aba158e7cfd283f7a67fccdca
ChenoGG <65572462+ChenoGG@users.noreply.github.com>
2024-09-14 01:54:54 +0200

    Add exercise 4

--- intro-javascript/Oppgave_04/src/litt_hjelp.js
+++ intro-javascript/Oppgave_04/src/litt_hjelp.js
@@ -1,4 +1,4 @@
-import './styles.css';
+//import './styles.css';
 
 // TODO: Bruk getElementById til å hente HTML med #id
 // TODO: Bruk querySelector til å hente knappen
@@ -6,7 +6,7 @@ const numbersUl = document.getElementById("numbers");
 const guessUl = document.getElementById("guess");
 const button = document.querySelector("button");
 // TODO: Lag en liste med tallene som skal sorteres
-const numbers = [2, 1, 8, 12, 4];
+const numbers = [2, 1, 8, 5, 4];
 
 const getGuess = () => {
   // TODO: Bruk querySelectorAll på guessUl for å hente ut alle input feltene
@@ -14,7 +14,7 @@ const getGuess = () => {
   // TODO: Bruk .map for å hente ut verdiene i input feltene og returner denne
   const input = guessUl.querySelectorAll("input");
   const inputToArray = Array.from(input);
-  const newInputArray = guessUl.map(inputToArray);
+  const newInputArray = inputToArray.map(input => input.value);
 
   return newInputArray;
 };
@@ -23,20 +23,41 @@ const checkNumberSeq = () => {
   // TODO: Lag logikken som sjekker om du har sortert riktig
   // Tips: Bruk getGuess() for å få "det som er gjettet"
   // Tips: Bruk .sort() .join()
+  const guess = getGuess();
+  if (numbers.sort().join(" ") === guess.join(" "))
+    alert("The numbers are sorted correctly.");
+  else 
+    alert("That's wrong, try again!");
 };
 
 const addInputUI = () => {
   // TODO: Bruk for-of (eller vanlig for-løkke) og guessUl til å lage li-elementer med input elementer for å kunne skrive inn hva som skal sorteres
+  for (let i = 0; i < numbers.length; i++) {
+    const li = document.createElement("li");
+    const input = document.createElement("input")
+
+    li.appendChild(input);
+    guessUl.appendChild(li);
+  }
 };
 
 const addNumbersUI = () => {
   // TODO: Bruk for-of (eller vanlig for-løkke) og numbersUl til å lage li-elementer med tallene som skal sorteres
+  for (let i = 0; i < numbers.length; i++) {
+    const li = document.createElement("li");
+    li.innerHTML = numbers[i];
+
+    numbersUl.appendChild(li);
+  }
 };
 
 const createUI = () => {
   // TODO: Bygg hele UI ved hjelp av UI funksjonene
+  addNumbersUI();
+  addInputUI(); 
 };
 
 createUI();
 
 // TODO: Lytt til 'click'-event og trigg validateNumberSeq ved klikk
+button.addEventListener("click", checkNumberSeq);
