3cdfdcf4245a9a92591711c1c22575a56b0043a2
ChenoGG <65572462+ChenoGG@users.noreply.github.com>
2024-09-16 15:00:29 +0200

    Add hono setup

---
+++ portfolio-v2/src/backend/src/index.ts
@@ -0,0 +1,31 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { serveStatic } from "@hono/node-server/serve-static";
+import { readFile } from "node:fs/promises";
+
+const app = new Hono();
+
+app.use("/*", cors())
+
+app.use("/statics/*", serveStatic({ root: "./" }))
+
+// /json is the web-route
+app.get("/json", async (c) => {
+    const data = await readFile("assets/data.json", "utf-8")
+
+    return c.json(JSON.parse(data))
+})
+
+// test
+app.get("/", async (c) => {
+    return c.text("Hellooooo am I breaking the 404?");
+});
+
+console.log("HOPE IT WORKS")
+
+const port = 3000
+serve({
+    fetch: app.fetch,
+    port,
+})
