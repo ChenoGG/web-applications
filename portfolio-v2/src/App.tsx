0cd860c636def9390a428db6aa53443e8cec944c
ChenoGG <65572462+ChenoGG@users.noreply.github.com>
2024-09-23 16:02:38 +0200

    Update app

--- portfolio-v2/src/App.tsx
+++ portfolio-v2/src/App.tsx
@@ -1,65 +1,34 @@
 import './styles.css';
-import Title from './components/Title';
-import Paragraph from './components/Paragraph';
 import Card from './components/Card';
 import HeaderPage from './components/HeaderPage';
 import AddProjectForm from './components/AddProjectForm';
 import OpenFormButton from './components/OpenFormButton';
+import CardListAll from './components/CardListAll';
 
 function App() {
+
   return (
      <>
         {/* Static images goes into public folder? */}
-        {/* Look for other ways instead of mapping it later.
-        I Just wanted two paragraphs man without two props. */}
+        {/* Look for other ways instead of mapping it later?
+        I Just wanted two paragraphs without using props for each man. */}
         <HeaderPage
           headerImage={{image: "assets/blue.png", 
             imageAltText: "pixelated-ish shades of blue in a cloud like pattern"}}
           title="Farming Arc"
-          paragraph={[`Did you know that farming transcends the mere thought of survival?
-            It is a metaphor for life, growth, and the pursuit of peace For the Glory of Mankind. 
-            To return to simplicity, to the being itself. 
-            It is not only cultivating the land, but the soul.
-            Each seed planted with the hopes of freeing the world from the scars of war.
-            Each harvest a testament to the unending resilience and hope.`, 
-            "A true warrior is not found in the sword, but the iridium hoe."]}
         />
 
       <main>
         <OpenFormButton />
-
         <AddProjectForm />
+        <CardListAll />
 
-        <div id="project-wrapper"></div>
-        <div id="project-wrapper-testing"></div> 
-
-        <Card 
+        {/* <Card 
           thumbnail={{image:"assets/farmland.jpg", imageAltText:"This is alt text"}}
           name="Project Zeus"
           language={["JavaScript", "React", "HTML"]}
-          description="The bad news is, it doesn't work. The good news is, maybe we shouldn't even be in sales!"
-        />
-
-        <Card 
-          thumbnail={{image:"assets/farmland.jpg", imageAltText:"This is alt text"}}
-          name="Project Zeus"
-          language={["JavaScript", "React", "HTML"]}
-          description="The bad news is, it doesn't work. The good news is, maybe we shouldn't even be in sales!"
-        />
-
-        <Card 
-          thumbnail={{image:"assets/farmland.jpg", imageAltText:"This is alt text"}}
-          name="Project Zeus"
-          language={["JavaScript", "React", "HTML"]}
-          description="The bad news is, it doesn't work. The good news is, maybe we shouldn't even be in sales!"
-        />
-
-        <Card 
-          thumbnail={{image:"assets/farmland.jpg", imageAltText:"This is alt text"}}
-          name="Project Zeus"
-          language={["JavaScript", "React", "HTML"]}
-          description="The bad news is, it doesn't work. The good news is, maybe we shouldn't even be in sales!"
-        />
+          description="The bad news is, it doesn't work. But perhaps we shouldn't be in the credit business at all."
+        /> */}
 
       </main>
     </>
