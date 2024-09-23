ad79e0a9b73ce60ef2c2c320148b12a77928d172
ChenoGG <65572462+ChenoGG@users.noreply.github.com>
2024-09-23 15:23:58 +0200

    fix: remove div as not needed

--- portfolio-v2/src/components/Card.tsx
+++ portfolio-v2/src/components/Card.tsx
@@ -4,20 +4,18 @@ type CardProps = {
     thumbnail: ImageProps
     name: string
     language: string[]
-    description: string 
+    description: string
 }
 
 export default function Card(props: CardProps) {
     const { thumbnail, name, language, description } = props
 
     return (
-        <div className="project-card-container">
             <section className="project-card">
                 <img src={thumbnail.image} alt={thumbnail.imageAltText} />
                 <h2>{name}</h2>
                 <p>{language.join(", ")}</p>
                 <p>{description}</p>
             </section>
-        </div>
     )
 }
