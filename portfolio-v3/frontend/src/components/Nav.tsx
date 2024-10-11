e30e47f735b023b948bb3fc4c959c209adcf86b4
ChenoGG <65572462+ChenoGG@users.noreply.github.com>
2024-10-11 13:56:26 +0200

    Add nav bar

---
+++ portfolio-v3/frontend/src/components/Nav.tsx
@@ -0,0 +1,10 @@
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="#">Extra</a></li>
+            </ul>
+        </nav>
+    )
+}
