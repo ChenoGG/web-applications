6e75c38061deb901974fddc55bd78683563db1a5
ChenoGG <65572462+ChenoGG@users.noreply.github.com>
2024-10-15 15:46:10 +0200

    Add confige file for variables

--- portfolio-v3/frontend/src/components/Header.tsx
+++ portfolio-v3/frontend/src/components/Header.tsx
@@ -1,19 +1,11 @@
 import Paragraph from "./Paragraph"
 import { ImageProps } from "./types"
+import { headerParagraphText, headerParagraphText2 } from "../configs/textContent"
 
 type HeaderProps = {
     headerImage: ImageProps
     title: string
 }
-
-const headerParagraphText = `Did you know that farming transcends the mere thought of survival?
-    It is a metaphor for life, growth, and the pursuit of peace For the Glory of Mankind. 
-    To return to simplicity, to the being itself. 
-    It is not only cultivating the land, but the soul.
-    Each seed planted with the hopes of freeing the world from the scars of war.
-    Each harvest a testament to the unending resilience and hope.`
-
-const headerParagraphText2 = "A true warrior is not found in the sword, but the iridium hoe."
 
 export default function Header(props: HeaderProps) {
     const { headerImage, title } = props
---
+++ portfolio-v3/frontend/src/configs/textContent.ts
@@ -0,0 +1,9 @@
+// HEADER CONTENT
+ export const headerParagraphText = `Did you know that farming transcends the mere thought of survival?
+    It is a metaphor for life, growth, and the pursuit of peace For the Glory of Mankind. 
+    To return to simplicity, to the being itself. 
+    It is not only cultivating the land, but the soul.
+    Each seed planted with the hopes of freeing the world from the scars of war.
+    Each harvest a testament to the unending resilience and hope.`
+
+export const headerParagraphText2 = "A true warrior is not found in the sword, but the iridium hoe."
