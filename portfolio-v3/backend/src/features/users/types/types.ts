0bd76b04e5c1f79250d3eb19c925c7b11ab21959
ChenoGG <65572462+ChenoGG@users.noreply.github.com>
2024-10-18 19:31:26 +0200

    Add project props

--- portfolio-v3/backend/src/features/users/types/types.ts
+++ portfolio-v3/backend/src/features/users/types/types.ts
@@ -3,3 +3,20 @@ export type User = {
     email: string;
     name: string;
 }
+
+export type ImageProps = {
+    image: string
+    imageAltText: string
+}
+
+export type ProjectProps = {
+    name: string
+    language: string[]
+    description: string
+    thumbnail: ImageProps
+    publishedAt: string | Date
+    isPublic: boolean
+    status: string
+    externalLinks?: string[]
+    tags: string[]
+}
