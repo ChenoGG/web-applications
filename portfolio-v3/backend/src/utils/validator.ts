b9cf27428cd7ad135ffab47e2a8933aaa1e96b76
ChenoGG <65572462+ChenoGG@users.noreply.github.com>
2024-10-30 23:37:46 +0100

    Add validation with zod for backend

---
+++ portfolio-v3/backend/src/utils/validator.ts
@@ -0,0 +1,31 @@
+import { ProjectProps } from "@/features/users/types/types";
+import { z } from "zod";
+
+const thumbnailSchema = z.object({
+    image: z.string().optional(),
+    imageAltText: z.string().optional(),
+})
+
+const projectSchema = z.object({
+    id: z.string().uuid(),
+    name: z.string().min(1, {message: "Project name is required"}), // https://stackoverflow.com/questions/75883100/how-to-make-a-custom-error-message-in-zod
+    language: z.array(z.string()).min(1, {message: "Project language is required"}),
+    description: z.string().optional(),
+    thumbnail: thumbnailSchema,
+    publishedAt: z.string().datetime().nullable().optional(),
+    isPublic: z.boolean(),
+    status: z.string().nullable().optional(),
+    externalLinks: z.array(z.string().url()).optional(), 
+    tags: z.array(z.string()).optional()
+})
+
+export const isValidProject = (data: Partial<ProjectProps>): boolean => {
+    
+        try {
+            projectSchema.parse(data)
+            return true
+
+        } catch (error) {
+            return false
+        }
+    }
